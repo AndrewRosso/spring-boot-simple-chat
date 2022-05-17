c837990ce844aada72c6225a0e7d277da34fdcb9
AndrewRosso <a.krasnov91@mail.ru>
2022-05-17 13:16:27 +0300

    config: webSocket & HttpHandshakeInterceptor

---
+++ src/main/java/com/example/simplechat/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.example.simplechat.config;
+
+import com.example.simplechat.interceptor.HttpHandshakeInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final HttpHandshakeInterceptor handshakeInterceptor;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .withSockJS()
+                .setInterceptors(handshakeInterceptor);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/topic");
+    }
+}
