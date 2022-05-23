b2956e400e7c2922a368272ee85b908765f47157
AndrewRosso <a.krasnov91@mail.ru>
2022-05-23 03:56:00 +0300

    feat: add simple validation

--- src/main/java/com/example/simplechat/entity/Member.java
+++ src/main/java/com/example/simplechat/entity/Member.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -16,19 +15,18 @@ public class Member {
 
     @Id
     @NotEmpty(message = "Login should not empty")
+    @Size(min = 2, max = 20, message = "Login should be between 2 and 20 characters")
     private String login;
 
     @NotEmpty(message = "First name should not empty")
-    @Size(min = 2, max = 20, message = "First name should be between 2 and 30 characters")
+    @Size(min = 2, max = 20, message = "First name should be between 2 and 20 characters")
     private String firstName;
 
     @NotEmpty(message = "Last name should not empty")
-    @Size(min = 2, max = 20, message = "Last name should be between 2 and 30 characters")
+    @Size(min = 2, max = 20, message = "Last name should be between 2 and 20 characters")
     private String lastName;
 
-    @NotNull
     @NotEmpty(message = "password should not empty")
-    @Size(min = 4, max = 16, message = "Password should be between 4 and 16 characters")
     private String password;
 
     @ManyToMany(cascade = CascadeType.ALL)
